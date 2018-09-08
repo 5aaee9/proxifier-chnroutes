9d65b913cb21746c862a5a05170dafcfb3204cb1
Indexyz <jiduye@gmail.com>
2018-09-08 21:04:10 +0800

    ref(index): update output

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const writeFile = promisify(fs.writeFile)
 const cidr = new CIDR()
 
 const RULE_COUNT = 10
+const DEFAULT_RULE = `\n    <Rule enabled="true">
+        <Name>Default</Name>
+        <Action type="Direct" />
+    </Rule>\n`
 
 async function main() {
     const source = new BgpSpaceSource()
@@ -16,7 +20,7 @@ async function main() {
     const preRuleCount = parseInt(((routes.length + RULE_COUNT) / RULE_COUNT)
         .toFixed(0))
 
-    let data = ''
+    let data = '<RuleList>\n'
 
     for (let i = 1; i <= RULE_COUNT; i++) {
         const name = `chnroute-${i}`
@@ -30,19 +34,20 @@ async function main() {
             .map(i => `${cidr.range(i).start}-${cidr.range(i).end}`)
             .join(';')
 
-        data += `
-        <Rule enable="true">
+        data += `        <Rule enable="true">
             <Name>${name}</Name>
             <Target>${routeText}</Target>
             <Action type="Direct" />
-        </Rule>
-        `
+        </Rule>\n`
     }
 
     // trim space
-    const result = data.split('\n')
-        .map(i => i.replace(/        /g, ''))
+    let result = data.split('\n')
+        .map(i => i.replace(/        /g, '    '))
         .join("\n")
+
+    result += DEFAULT_RULE
+    result += '</RuleList>'
 
     await writeFile('chnroute.xml', result)
 }
